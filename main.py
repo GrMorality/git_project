82e11c3e53da7ae9cce45829c25aa5e461049afd
GrMorality <jmihailys@gmail.com>
2024-06-05 00:14:57 +1000

    added: subtract_function

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from funcs import sum_function
+from funcs import sum_function, subtract_function
 
 a = int(input("a = "))
 b = int(input("b = "))
 
 
 def main():
-    result = sum_function(a, b)
+    print('press "1" to sum, press "2" to subtract\n')
+    if int(input('Press "1" to sum, press "2" to subtract\n')) == 1:
+        result = sum_function(a, b)
+    else:
+        result = subtract_function(a, b)
     print(result)
 
 
