72b576a735bf7c8100fb621b8bb5f77d5db53303
GrMorality <jmihailys@gmail.com>
2024-06-05 00:02:48 +1000

    added:
    from funcs import sum_function
    
    a = int(input("a = "))
    b = int(input("b = "))
    
    def main():
        result = sum_function(a, b)
        print(result)
    
    if __name__ == "__main__":
        main()

---
+++ main.py
@@ -0,0 +1,13 @@
+from funcs import sum_function
+
+a = int(input("a = "))
+b = int(input("b = "))
+
+
+def main():
+    result = sum_function(a, b)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
